2b7d4e1e8fa58719230106f0df4758b7e3d07995
Daniel Santander <danyvo1d@gmail.com>
2020-03-20 17:00:50 -0600

    added result method and fixed rubocop offenses

--- bin/main.rb
+++ bin/main.rb
@@ -23,28 +23,40 @@ end
 
 def turn(player)
   puts "#{player.name}, choose your cell!"
-    pick = gets.chomp
-    cell = pick.to_i - 1
+  pick = gets.chomp
+  cell = pick.to_i - 1
 
-    if @board.arr[cell] == X || @board.arr[cell] == O || !pick[/\d/] 
-      puts 'Invalid cell, please choose a number between 1 and 9!'
-      @board.print
-      self.turn(player)
-    else
-      puts "#{player.chip} on cell #{pick}!"
-      @board.arr[cell] = player.chip
-      @board.print
-    end
+  if @board.arr[cell] == X || @board.arr[cell] == O || !pick[/\d/]
+    puts 'Invalid cell, please choose a number between 1 and 9!'
+    @board.print
+    turn(player)
+  else
+    puts "#{player.chip} on cell #{pick}!"
+    @board.arr[cell] = player.chip
+    @board.print
+  end
 end
 
-def play (player1, player2)
+def play(player1, player2)
   @board = Board.new
   @board.print
 
   until @board.full? || @board.win
-    t1 = turn(player1)
+    turn(player1)
     break if @board.full? || @board.win
-    t2 = turn(player2)
+
+    turn(player2)
+  end
+end
+
+def results
+  # needs finnesing
+  if @board.win
+    puts "And the winner is: #{@board.win}!  You rock!"
+    @board.print
+  elsif @board.full?
+    puts "It's a draw! #{p1.chip}, #{p2.chip} what a wonderful battle of wits!"
+    @board.print
   end
 end
 
@@ -62,54 +74,30 @@ puts ''
 
 puts 'Player one, type in your name:'
 p1_name = gets.chomp
-name1 = valid_name(p1_name) 
-X = "X".green
+name1 = valid_name(p1_name)
+X = 'X'.green
 p1 = Player.new(name1, X)
 puts "#{p1.name}, you chose #{p1.chip} as your weapon!"
 
 puts 'Player two, type in your name:'
 p2_name = gets.chomp
-name2 = valid_name(p2_name) 
-O = "O".blue
+name2 = valid_name(p2_name)
+O = 'O'.blue
 p2 = Player.new(name2, O)
 puts "#{p2.name}, you chose #{p2.chip} as your weapon!"
 
 puts "#{p1_name} vs #{p2_name}"
 
-self.play(p1, p2)
-
-
-playing = true
-
-while playing
-  puts "#{p1_name}, choose your cell!"
-  p1_play = gets.chomp
-  puts "#{p1_symbol} on cell #{p1_play}!"
-
-  puts "#{p2_name}, choose your cell!"
-  p2_play = gets.chomp
-  puts "#{p2_symbol} on cell #{p2_play}!"
-
-  puts 'The game so far...'
-  puts '###UPDATED BOARD HERE###'
-
-  if resolution == 'win'
-    playing = false
-    puts "Winner is: #{winner_name}. #{winner_symbol} reigns!"
-    puts '###FINAL BOARD###'
-  elsif resolution == 'draw'
-    playing = false
-    puts "It's a draw! #{p1_symbol}, #{p2_symbol} what a wonderful battle of wits!"
-    puts '###FINAL BOARD###'
-  end
-end
+play(p1, p2)
+# method for showing results
 
 puts 'You wanna play again? [y/n]'
 play_again = gets.chomp
 valid_input = valid_play_again(play_again)
-unless valid_input
+
+while valid_input
   if play_again == 'y'
-  # initialize board array
+  # initialize game
   else
     puts 'Ok! See ya!'
   end
