680175ab03a5c04dbd53fa729cee290e2076fea2
Rodolfo Mata <rodmatrod@gmail.com>
2020-03-19 22:00:55 -0600

    Add turn loop trial

--- bin/main.rb
+++ bin/main.rb
@@ -4,7 +4,7 @@ puts " "
 puts "Instructions:"
 puts "-Each player chooses a name."
 puts "-Each player chooses a symbol (X and O are the all-time classics!)"
-puts "-The board is displayed! a 3x3 cells array!"
+puts "-The board is displayed! a 3x3 cells array! Choose by number!"
 puts "###BOARD APPEARS HERE###"
 puts "-Players take turns to choose a cell, the first one to make a line of symbols wins!"
 puts "***Have fun!***"
@@ -25,3 +25,28 @@ puts "#{p2_name}, you chose #{p2_symbol} as your weapon!"
 
 puts "#{p1_name} vs #{p2_name}"
 puts "###BOARD APPEARS HERE###"
+
+playing = true
+
+while playing
+  puts "#{p1_name}, choose your cell!"
+  p1_play = gets.chomp
+  puts "#{p1_symbol} on cell #{p1_play}!"
+
+  puts "#{p2_name}, choose your cell!"
+  p2_play = gets.chomp
+  puts "#{p2_symbol} on cell #{p2_play}!"
+
+  puts "The game so far..."
+  puts "###UPDATED BOARD HERE###"
+
+  if resolution == "win"
+    playing = false
+    puts "Winner is: #{winner_name}. #{winner_symbol} reigns!"
+    puts "###FINAL BOARD###"
+  elsif resolution == "draw"
+    playing = false
+    puts "It's a draw! #{p1_symbol}, #{p2_symbol} what a wonderful battle of wits!"
+    puts "###FINAL BOARD###"
+  end
+end
