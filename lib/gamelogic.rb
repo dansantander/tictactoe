a3d0d5a6ea6600b356c3670f75d554d0e61bef8f
Daniel Santander <danyvo1d@gmail.com>
2020-03-25 16:18:37 -0600

    Separated methods in game logic class

--- lib/gamelogic.rb
+++ lib/gamelogic.rb
@@ -1,11 +1,20 @@
 class GameLogic
-  def score(player1, player2, board)
+  
+  def winner(player1, player2, board)
     if board.win == 1
-      player1.score += 1
       player1.name.green
     elsif board.win == 2
-      player2.score += 1
       player2.name.blue
+    else
+      false
+    end
+  end
+
+  def score (player1, player2, board)
+    if board.win == 1
+      player1.score += 1
+    elsif board.win == 2
+      player2.score += 1
     else
       false
     end
