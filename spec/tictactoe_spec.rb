bee0d8751ec3fc856c72907d317c364fd19df439
Daniel Santander <danyvo1d@gmail.com>
2020-03-26 09:05:20 -0600

    Added missing cases for #win

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -10,55 +10,95 @@ describe Board do
   O = 'O'.blue
 
   describe '#win' do
-    it 'checks win for three marks across top row' do
-      board.arr = [X, X, X]
+    it 'checks win for three X marks across top row' do
+      board.arr = [X, X, X, 4, 5, 6, 7, 8, 9]
       expect(board.win).to eql(1)
     end
 
-    it 'checks win for three marks across middle row' do
+    it 'checks win for three X marks across middle row' do
       board.arr = [1, 2, 3, X, X, X, 7, 8, 9]
       expect(board.win).to eql(1)
     end
 
-    it 'checks win for three marks across bottom row' do
+    it 'checks win for three X marks across bottom row' do
       board.arr = [1, 2, 3, 4, 5, 6, X, X, X]
       expect(board.win).to eql(1)
     end
 
-    it 'checks win for three marks across left column' do
+    it 'checks win for three X marks across left column' do
       board.arr = [X, 2, 3, X, 5, 6, X, 8, 9]
       expect(board.win).to eql(1)
     end
 
-    it 'checks win for three marks across middle column' do
+    it 'checks win for three X marks across middle column' do
       board.arr = [1, X, 3, 4, X, 6, 7, X, 9]
       expect(board.win).to eql(1)
     end
 
-    it 'checks win for three marks across right column' do
+    it 'checks win for three X marks across right column' do
       board.arr = [1, 2, X, 4, 5, X, 7, 8, X]
       expect(board.win).to eql(1)
     end
 
-    it 'checks win for three marks across right to left diagonal' do
+    it 'checks win for three X marks across right to left diagonal' do
       board.arr = [1, 2, X, 4, X, 6, X, 8, 9]
       expect(board.win).to eql(1)
     end
 
-    it 'checks win for three marks across left to right diagonal' do
+    it 'checks win for three X marks across left to right diagonal' do
       board.arr = [X, 2, 3, 4, X, 6, 7, 8, X]
       expect(board.win).to eql(1)
     end
 
-    it 'returns false if none of the players won)' do
+    it 'checks win for three O marks across top row' do
+      board.arr = [O, O, O, 4, 5, 6, 7, 8, 9]
+      expect(board.win).to eql(2)
+    end
+
+    it 'checks win for three O marks across middle row' do
+      board.arr = [1, 2, 3, O, O, O, 7, 8, 9]
+      expect(board.win).to eql(2)
+    end
+
+    it 'checks win for three O marks across bottom row' do
+      board.arr = [1, 2, 3, 4, 5, 6, O, O, O]
+      expect(board.win).to eql(2)
+    end
+
+    it 'checks win for three O marks across left column' do
+      board.arr = [O, 2, 3, O, 5, 6, O, 8, 9]
+      expect(board.win).to eql(2)
+    end
+
+    it 'checks win for three O marks across middle column' do
+      board.arr = [1, O, 3, 4, O, 6, 7, O, 9]
+      expect(board.win).to eql(2)
+    end
+
+    it 'checks win for three O marks across right column' do
+      board.arr = [1, 2, O, 4, 5, O, 7, 8, O]
+      expect(board.win).to eql(2)
+    end
+
+    it 'checks win for three O marks across right to left diagonal' do
+      board.arr = [1, 2, O, 4, O, 6, O, 8, 9]
+      expect(board.win).to eql(2)
+    end
+
+    it 'checks win for three O marks across left to right diagonal' do
+      board.arr = [O, 2, 3, 4, O, 6, 7, 8, O]
+      expect(board.win).to eql(2)
+    end
+
+    it 'returns false if none of the players won' do
       board.arr = [X, O, X, X, O, O, O, X, X]
       expect(board.win).to eql(false)
     end
   end
 
-  describe 'full?' do
+  describe '#full?' do
     it 'checks if the board is full (none of the players won)' do
-      board.arr = [X, O, X, X, O, O, O, X, X]
+      board.arr = [X, O, X, X, O, O, O, X, X] # tie case
       expect(board.full?).to eql(true)
     end
 
@@ -98,6 +138,11 @@ describe GameLogic do
     it "returns which player wins the match in it's due color" do
       board.arr = [O, O, O, X, O, X, X, X, O] # player 2 wins
       expect(game.winner(p1, p2, board)).to eql(p2.name.blue)
+    end
+
+    it "returns false if none of the players won the match" do
+      board.arr = [X, O, X, X, O, O, O, X, X] # tie case
+      expect(game.winner(p1, p2, board)).to eql(false)
     end
   end
 
